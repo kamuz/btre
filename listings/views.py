54dcd182ff7b2c9401af09301da497c81ed7d922
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-05-15 00:00:30 +0300

    Show listings if are published

--- listings/views.py
+++ listings/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from listings.models import Listing
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     paginator = Paginator(listings, 3)
     page = request.GET.get('page')
