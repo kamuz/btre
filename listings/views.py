ae91245304a177853c53d08a9e3e12fae37d775f
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-06-16 00:53:53 +0300

    Search query

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from listings.models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
 
@@ -27,4 +28,48 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    # Keywords contains words
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            listings = listings.filter(description__icontains=keywords)
+
+    # City exactly
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            listings = listings.filter(city__iexact=city)
+
+    # State exactly
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            listings = listings.filter(state__iexact=state)
+
+    # Bedrooms grater then
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            listings = listings.filter(bedrooms__gte=bedrooms)
+
+    # Price less then
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            listings = listings.filter(price__lte=price)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+    }
+
+    return render(request, 'listings/search.html', context)
